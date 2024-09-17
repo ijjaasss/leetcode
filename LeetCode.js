74b85dd3b93ba4ec1f212d020245e53ce2bdcee9
ijjaasss <ijasmuthu04@gmail.com>
2024-09-17 10:44:23 +0530

    Day 3

--- LeetCode.js
+++ LeetCode.js
@@ -26,6 +26,10 @@
 
 
 
+
+
+
+
 //2
 // Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
 
@@ -54,24 +58,71 @@
 
 
 
-var expect = function(val) {
-    function toBe(n){
-        if(val===n){
-            return true
-        }
-        else{
-            throw new Error('Not Equal')
-        }
-    }
-    function notToBe(n){
-        if(val!==n){
-            return true
-        }else{
-            throw new Error('Equal')
-        }
-    }
-    return {
-        toBe,
-        notToBe
-    }
+// var expect = function(val) {
+//     function toBe(n){
+//         if(val===n){
+//             return true
+//         }
+//         else{
+//             throw new Error('Not Equal')
+//         }
+//     }
+//     function notToBe(n){
+//         if(val!==n){
+//             return true
+//         }else{
+//             throw new Error('Equal')
+//         }
+//     }
+//     return {
+//         toBe,
+//         notToBe
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+ 
+
+// Example 1:
+
+// Input: 
+// n = 10 
+// ["call","call","call"]
+// Output: [10,11,12]
+// Explanation: 
+// counter() = 10 // The first time counter() is called, it returns n.
+// counter() = 11 // Returns 1 more than the previous time.
+// counter() = 12 // Returns 1 more than the previous time.
+
+// Example 2:
+
+// Input: 
+// n = -2
+// ["call","call","call","call","call"]
+// Output: [-2,-1,0,1,2]
+// Explanation: counter() initially returns -2. Then increases after each sebsequent call.
+
+
+
+var createCounter = function(n) {
+    
+    return function() {
+   
+   return n++;
+    };
 };
