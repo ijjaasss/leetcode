a70fb9ae473ee6843e66be3eca35e2b5a11e58db
ijjaasss <ijasmuthu04@gmail.com>
2024-09-20 17:13:54 +0530

    Day 5

--- LeetCode.js
+++ LeetCode.js
@@ -150,42 +150,93 @@
 
  
 
-// Example 1:
+// // Example 1:
 
-// Input: init = 5, calls = ["increment","reset","decrement"]
-// Output: [6,5,4]
-// Explanation:
-// const counter = createCounter(5);
-// counter.increment(); // 6
-// counter.reset(); // 5
-// counter.decrement(); // 4
-/**
- * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
- */
-var createCounter = function(init) {
-    let temp=init
-    function increment(){
-        return ++init
-    }
-    function decrement(){
-        return --init
-    }
-    function reset(){
-        init=temp
-        return init
-    }
-     return {
-        reset,decrement,increment
-    }
+// // Input: init = 5, calls = ["increment","reset","decrement"]
+// // Output: [6,5,4]
+// // Explanation:
+// // const counter = createCounter(5);
+// // counter.increment(); // 6
+// // counter.reset(); // 5
+// // counter.decrement(); // 4
+// /**
+//  * @param {integer} init
+//  * @return { increment: Function, decrement: Function, reset: Function }
+//  */
+// var createCounter = function(init) {
+//     let temp=init
+//     function increment(){
+//         return ++init
+//     }
+//     function decrement(){
+//         return --init
+//     }
+//     function reset(){
+//         init=temp
+//         return init
+//     }
+//      return {
+//         reset,decrement,increment
+//     }
    
 
 
-};
+// };
+
+// /**
+//  * const counter = createCounter(5)
+//  * counter.increment(); // 6
+//  * counter.reset(); // 5
+//  * counter.decrement(); // 4
+//  */
+
+
+
+
+
+
+
+
+//5:-
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+// Output: [2,3,4]
+// Explanation:
+// const newArray = map(arr, plusone); // [2,3,4]
+// The function increases each value in the array by one. 
+
+// Example 2:
+
+// Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+// Output: [1,3,5]
+// Explanation: The function increases each value by the index it resides in.
+
+// Example 3:
+
+// Input: arr = [10,20,30], fn = function constant() { return 42; }
+// Output: [42,42,42]
+// Explanation: The function always returns 42
+
 
 /**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
  */
+var map = function(arr, fn) {
+    var result=[]
+      for (let i = 0; i < arr.length; i++) {
+         
+          result.push(fn(arr[i],i));
+      }
+     return result
+  };
