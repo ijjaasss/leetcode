cc58dc9da2cfd88d334e9a99266bcfa09abb530f
ijjaasss <ijasmuthu04@gmail.com>
2024-09-23 11:15:12 +0530

    7

--- LeetCode.js
+++ LeetCode.js
@@ -289,18 +289,79 @@
 // Falsey values such as 0 should be filtered out
 
  
-/**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
- */
-var filter = function(arr, fn) {
-    var result=[]
-    for (let i = 0; i < arr.length; i++) {
+// /**
+//  * @param {number[]} arr
+//  * @param {Function} fn
+//  * @return {number[]}
+//  */
+// var filter = function(arr, fn) {
+//     var result=[]
+//     for (let i = 0; i < arr.length; i++) {
        
-          if (fn(arr[i], i)) {
-            result.push(arr[i]); 
+//           if (fn(arr[i], i)) {
+//             result.push(arr[i]); 
+//         }
+//     }
+//    return result
+// };
+
+
+
+
+
+
+
+
+
+//7:-
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+//     The first time the returned function is called, it should return the same result as fn.
+//     Every subsequent time it is called, it should return undefined.
+
+ 
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+
+// Example 2:
+
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    c=0
+    return function(...args){
+        if(c==0){
+            c++;
+            return fn(...args)
         }
+       
     }
-   return result
 };
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
+
+
+ 
