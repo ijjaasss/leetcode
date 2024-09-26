a213978e536bb5dff424d8781dfb47bfbcd0ff85
ijjaasss <ijasmuthu04@gmail.com>
2024-09-26 09:30:00 +0530

    Describe your changes here

--- LeetCode.js
+++ LeetCode.js
@@ -340,28 +340,80 @@
 // onceFn(2, 3, 6); // undefined, fn was not called
 // onceFn(4, 6, 8); // undefined, fn was not called
 
-/**
- * @param {Function} fn
- * @return {Function}
- */
-var once = function(fn) {
-    c=0
-    return function(...args){
-        if(c==0){
-            c++;
-            return fn(...args)
-        }
+// /**
+//  * @param {Function} fn
+//  * @return {Function}
+//  */
+// var once = function(fn) {
+//     c=0
+//     return function(...args){
+//         if(c==0){
+//             c++;
+//             return fn(...args)
+//         }
        
-    }
+//     }
+// };
+
+// /**
+//  * let fn = (a,b,c) => (a + b + c)
+//  * let onceFn = once(fn)
+//  *
+//  * onceFn(1,2,3); // 6
+//  * onceFn(2,3,6); // returns undefined without calling fn
+//  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//8
+// Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+
+ 
+
+// Example 1:
+
+// Input: 
+// promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)), 
+// promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+// Output: 7
+// Explanation: The two input promises resolve with the values of 2 and 5 respectively. The returned promise should resolve with a value of 2 + 5 = 7. The time the returned promise resolves is not judged for this problem.
+
+// Example 2:
+
+// Input: 
+// promise1 = new Promise(resolve => setTimeout(() => resolve(10), 50)), 
+// promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30))
+// Output: -2
+// Explanation: The two input promises resolve with the values of 10 and -12 respectively. The returned promise should resolve with a value of 10 + -12 = -2.
+
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    var a=await promise1
+    var b=await promise2
+    return a+b
 };
 
 /**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
  */
+
 
 
  
