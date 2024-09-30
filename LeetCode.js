ed14d98f272527f8ad19ab90ded5eb47123d331f
ijjaasss <ijasmuthu04@gmail.com>
2024-09-30 10:12:49 +0530

    11

--- LeetCode.js
+++ LeetCode.js
@@ -469,6 +469,11 @@
 //  * arr.last(); // 3
 //  */
 
+
+
+
+
+
 //10
 // Given an integer x, return true if x is a
 // palindrome
@@ -494,18 +499,152 @@
 // Output: false
 // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
-    var a=x
-    var b=a.toString()
- var result =0
- for(let x=b.length-1;x>=0;x--){
-     result+=b[x]
- }
+// /**
+//  * @param {number} x
+//  * @return {boolean}
+//  */
+// var isPalindrome = function(x) {
+//     var a=x
+//     var b=a.toString()
+//  var result =0
+//  for(let x=b.length-1;x>=0;x--){
+//      result+=b[x]
+//  }
  
     
-     return result==a
- };
+//      return result==a
+//  };
+
+
+
+
+
+
+
+
+
+//11
+// Editorial
+// Editorial
+// Solutions
+// Solutions
+// Submissions
+// Submissions
+// Code
+// Testcase
+// Test Result
+// Test Result
+// 13. Roman to Integer
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+//     I can be placed before V (5) and X (10) to make 4 and 9. 
+//     X can be placed before L (50) and C (100) to make 40 and 90. 
+//     C can be placed before D (500) and M (1000) to make 400 and 900.
+
+// Given a roman numeral, convert it to an integer.
+
+ 
+
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+
+// Example 2:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+
+// Example 3:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var result =0
+    for(let i=0;i<s.length;i++){
+        if(s[i]=="I"){
+            if(s[i+1]=="V"||s[i+1]=="X"){
+                continue;
+            }else{
+            result+=1
+            }
+           
+        }else if(s[i]=="V"){
+            if(s[i-1]=='I'){
+               result+=4 
+            }else{
+              result+=5
+            }
+            
+        }else if(s[i]=="X"){
+            if(s[i+1]=="L"||s[i+1]=="C"){
+                continue  ;
+            }else if(s[i-1]=='I'){
+                result+=9
+            }else{
+                result+=10
+            }
+            
+        }else if(s[i]=="L"){
+            if(s[i-1]=='X'){
+               result+=40 
+            }else{
+                 result+=50
+            }
+           
+        }else if(s[i]=="C"){
+            if(s[i+1]=="D"||s[i+1]=="M"){
+                continue;
+            }else if(s[i-1]=='X'){
+               result+=90 
+            }else{
+                result+=100
+            }
+            
+        }else if(s[i]=="D"){
+            if(s[i-1]=='C'){
+               result+=400 
+            }else{
+                result+=500
+            }
+            
+        }else if(s[i]=="M"){
+            if(s[i-1]=="C"){
+                result+=900
+            }else{
+                result+=1000
+            }
+            
+        }
+    }
+    return result
+};
+
+ 
