1d57e7b54d97a7f9cd8cf9f42fb70f820543eceb
ijjaasss <ijasmuthu04@gmail.com>
2024-10-31 12:25:40 +0530

    24

--- LeetCode.js
+++ LeetCode.js
@@ -1120,24 +1120,94 @@ var isAnagram = function(s, t) {
 
 // Input: num = "52"
 // Output: "5"
-/**
+// /**
+//  * @param {string} num
+//  * @return {string}
+//  */
+// var largestOddNumber = function(num) {
+    
+//   var result = "";
+//   var l = -1; 
+  
+//   for (let i = num.length - 1; i >= 0; i--) {
+//       if (parseInt(num[i]) % 2 !== 0) {
+//           l = i; 
+//           break; 
+//       }
+//   }
+  
+//   if (l !== -1) { 
+//       result += num.slice(0, l + 1); 
+//   }
+//   return result
+//   };
+
+
+
+
+
+// //24
+// You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+//     It is a substring of num with length 3.
+//     It consists of only one unique digit.
+
+// Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
+// Note:
+
+//     A substring is a contiguous sequence of characters within a string.
+//     There may be leading zeroes in num or a good integer.
+
+ 
+
+// Example 1:
+
+// Input: num = "6777133339"
+// Output: "777"
+// Explanation: There are two distinct good integers: "777" and "333".
+// "777" is the largest, so we return "777".
+
+// Example 2:
+
+// Input: num = "2300019"
+// Output: "000"
+// Explanation: "000" is the only good integer.
+
+// Example 3:
+
+// Input: num = "42352338"
+// Output: ""
+// Explanation: No substring of length 3 consists of only one unique digit. Therefore, there are no good integers.
+
+ /**
  * @param {string} num
  * @return {string}
  */
-var largestOddNumber = function(num) {
+var largestGoodInteger = function(num) {
+    var matche=num.match(/(\d)\1+/g)
+    if(matche==null){
+        return ""
+    }
+var result=[]
+for(let i=0;i<matche.length;i++){
+  if(matche[i].length==3){
+    result.push(matche[i])
+  }else if(matche[i].length>3){
+   result.push(matche[i].slice(0,3))
     
-  var result = "";
-  var l = -1; 
-  
-  for (let i = num.length - 1; i >= 0; i--) {
-      if (parseInt(num[i]) % 2 !== 0) {
-          l = i; 
-          break; 
-      }
   }
   
-  if (l !== -1) { 
-      result += num.slice(0, l + 1); 
-  }
-  return result
-  };
+}
+if(result.length==0){
+  
+    return ""
+}
+var res=Math.max(...result)
+if (res==0){
+    return '000'
+}
+return res.toString()
+
+
+};
