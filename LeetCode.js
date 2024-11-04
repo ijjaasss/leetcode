c5f1336a07b2581835c0bd950132db90b28d7fec
ijjaasss <ijasmuthu04@gmail.com>
2024-11-04 17:27:36 +0530

    27

--- LeetCode.js
+++ LeetCode.js
@@ -1295,3 +1295,28 @@ var removeElement = function(nums, val) {
  }
 return j
 };
+
+
+
+
+// //27
+// Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+// To separate the digits of an integer is to get all the digits it has in the same order.
+
+//     For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+
+ 
+
+// Example 1:
+
+// Input: nums = [13,25,83,77]
+// Output: [1,3,2,5,8,3,7,7]
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var separateDigits = function(nums) {
+   return nums.join('').split('').map((val)=>Number(val))
+};
