5ffed1d18dc485f1d696a427eac6ad89938d8fcd
ijjaasss <ijasmuthu04@gmail.com>
2024-11-04 17:47:41 +0530

    28

--- LeetCode.js
+++ LeetCode.js
@@ -1320,3 +1320,45 @@ return j
 var separateDigits = function(nums) {
    return nums.join('').split('').map((val)=>Number(val))
 };
+
+
+//28
+// You are given a positive integer n. Each digit of n has a sign according to the following rules:
+
+//     The most significant digit is assigned a positive sign.
+//     Each other digit has an opposite sign to its adjacent digits.
+
+// Return the sum of all digits with their corresponding sign.
+
+ 
+
+// Example 1:
+
+// Input: n = 521
+// Output: 4
+// Explanation: (+5) + (-2) + (+1) = 4.
+
+// Example 2:
+
+// Input: n = 111
+// Output: 1
+// Explanation: (+1) + (-1) + (+1) = 1.
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var alternateDigitSum = function(n) {
+   var num=n.toString()
+var tem=true
+var sum=0
+for(let i=0;i<num.length;i++){
+ if(tem){
+  sum+=Number(num[i])
+  tem=!tem
+ }else{
+   sum-=Number(num[i])
+   tem=!tem
+ }
+}
+return sum
+};
