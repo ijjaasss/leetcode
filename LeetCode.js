07c3ae17f2928e17e25bfc73c86eb26fae28e80b
ijjaasss <ijasmuthu04@gmail.com>
2024-11-04 18:08:15 +0530

    28

--- LeetCode.js
+++ LeetCode.js
@@ -1362,3 +1362,41 @@ for(let i=0;i<num.length;i++){
 }
 return sum
 };
+
+
+//28
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+// Example 1:
+
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+   var n=num.toString()
+   var sum=0
+for(let i=0;i<Number(n[0]);i+=0){
+   sum=n.split('').reduce((acc,val)=>acc+Number(val),0)
+
+   if(sum.toString().length==1){
+       break;
+       
+   }else{
+       n=sum.toString()
+      
+   }
+
+
+
+}
+return sum
+};
