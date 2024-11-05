64697b58150f1278c538c84f58d4f6c4d6605433
ijjaasss <ijasmuthu04@gmail.com>
2024-11-05 15:56:41 +0530

    28

--- LeetCode.js
+++ LeetCode.js
@@ -1400,3 +1400,38 @@ for(let i=0;i<Number(n[0]);i+=0){
 }
 return sum
 };
+
+
+
+
+//28
+// Given a 2D integer array matrix, return the transpose of matrix.
+
+// The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+// Example 1:
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+// Example 2:
+
+// Input: matrix = [[1,2,3],[4,5,6]]
+// Output: [[1,4],[2,5],[3,6]]
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function(matrix) {
+   var res=[]
+
+ for(let i=0;i<matrix[0].length;i++){
+     var temp=[]
+   for(let j=0;j<matrix.length;j++){
+         temp.push(matrix[j][i])
+   }
+   res.push(temp)
+   
+ }
+ return res
+};
