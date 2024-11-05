f42ef08cc7478635b66c3a3273a1434d56fc356f
ijjaasss <ijasmuthu04@gmail.com>
2024-11-05 16:19:22 +0530

    29

--- LeetCode.js
+++ LeetCode.js
@@ -1435,3 +1435,53 @@ var transpose = function(matrix) {
  }
  return res
 };
+
+
+
+//29
+// Given a square matrix mat, return the sum of the matrix diagonals.
+
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+// Input: mat = [[1,2,3],
+// [4,5,6],
+// [7,8,9]]
+// Output: 25
+// Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+// // Notice that element mat[1][1] = 5 is counted only once.
+// Example 2:
+
+// Input: mat = [[1,1,1,1],
+//               [1,1,1,1],
+//               [1,1,1,1],
+//               [1,1,1,1]]
+// Output: 8
+
+
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+   var c=0
+var l=mat[0].length-1
+
+var res=[]
+for(let i=0;i<mat.length;i++){
+   
+
+if(l!=c){
+   res.push(mat[i][c])
+   res.push(mat[i][l])
+}else {
+res.push(mat[i][l])
+}
+   l--;
+
+   c++
+}
+var sum=res.reduce((acc,val)=>acc+val,0)
+return sum
+
+
+};
