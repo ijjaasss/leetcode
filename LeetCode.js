894cc206d808a1d287ce09027b2b1d441f7ebac3
ijjaasss <ijasmuthu04@gmail.com>
2024-11-07 15:14:34 +0530

    32

--- LeetCode.js
+++ LeetCode.js
@@ -1654,3 +1654,33 @@ return po[0]
 }
 
 };
+
+
+
+
+//32
+// Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+
+// The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.  
+
+ 
+
+// Example 1:
+
+// Input: n = 4
+// Output: "pppz"
+// Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var generateTheString = function(n) {
+   var str=''
+  if(n%2!=0){
+   str=str.padStart(n,'a')
+  }else{
+   str=str.padStart(n-1,'a')
+   str+='b'
+  }
+  return str
+};
