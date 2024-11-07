b2aa4145bc7214e07ceb9ca756f05138860b46cc
ijjaasss <ijasmuthu04@gmail.com>
2024-11-07 17:11:15 +0530

    34

--- LeetCode.js
+++ LeetCode.js
@@ -1711,3 +1711,57 @@ var countOdds = function(low, high) {
    }
    return c
 };
+
+
+//34
+// Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+
+// A subarray is a contiguous subsequence of the array.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,4,2,5,3]
+// Output: 58
+// Explanation: The odd-length subarrays of arr and their sums are:
+// [1] = 1
+// [4] = 4
+// [2] = 2
+// [5] = 5
+// [3] = 3
+// [1,4,2] = 7
+// [4,2,5] = 11
+// [2,5,3] = 10
+// [1,4,2,5,3] = 15
+// If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumOddLengthSubarrays = function(arr) {
+   var res=[]
+var l=arr.length/2
+l=Math.ceil(l)
+
+for(let i=1;i<=arr.length;i+=2){
+   var c=i
+   for(let j=0;j<arr.length;j++){
+       var temp=arr.slice(j,c)
+       c++
+       
+      
+      if(temp.length!==i){
+       break
+      }
+     res.push(temp)
+      
+       
+   }
+   
+}
+res=res.flat()
+var sum=res.reduce((acc,val)=>acc+val,0)
+
+return sum
+};
