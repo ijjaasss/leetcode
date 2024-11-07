4f419d015c8b9a5be4bb97025300c60c3c5e2d39
ijjaasss <ijasmuthu04@gmail.com>
2024-11-07 15:19:05 +0530

    33

--- LeetCode.js
+++ LeetCode.js
@@ -1684,3 +1684,30 @@ var generateTheString = function(n) {
   }
   return str
 };
+
+
+//33
+// Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+ 
+
+// Example 1:
+
+// Input: low = 3, high = 7
+// Output: 3
+// Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+   var c=0
+   for(let i=low;i<=high;i++){
+       if(i%2!=0){
+           c++
+       }
+   }
+   return c
+};
