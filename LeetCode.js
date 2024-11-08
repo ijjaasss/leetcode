8f4a443056ab6e327d166fc95961abd91dc2fe33
ijjaasss <ijasmuthu04@gmail.com>
2024-11-08 09:53:55 +0530

    35

--- LeetCode.js
+++ LeetCode.js
@@ -1765,3 +1765,39 @@ var sum=res.reduce((acc,val)=>acc+val,0)
 
 return sum
 };
+
+
+
+
+
+
+
+//35
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+// You must solve this problem without using the library's sort function.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+   var res=[]
+      for(let i=0;i<nums.length;i+=0){
+         var a=Math.min(...nums)
+         var b=nums.indexOf(a)
+         nums.splice(b,1)
+         res.push(a)
+      }
+  for(let i=0;i<res.length;i++){
+      nums[i]=res[i]
+  }
+  };
