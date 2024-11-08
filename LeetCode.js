b0bb97b636d9818e254502ed5b584ec96aa7c60f
ijjaasss <ijasmuthu04@gmail.com>
2024-11-08 10:12:22 +0530

    36

--- LeetCode.js
+++ LeetCode.js
@@ -1801,3 +1801,38 @@ var sortColors = function(nums) {
       nums[i]=res[i]
   }
   };
+
+
+
+  //36
+//   Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+// Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+
+ 
+
+// Example 1:
+
+// Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+// Output: [2,2,2,1,4,3,3,9,6,7,19]
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+var relativeSortArray = function(arr1, arr2) {
+   var notarray=arr1.filter((val)=>!arr2.includes(val)).sort((a,b)=>a-b)
+
+var res=[]
+for(let i=0;i<arr2.length;i++){
+   for(let j=0;j<arr1.length;j++){
+       if(arr2[i]==arr1[j]){
+           res.push(arr1[j])
+       }
+   }
+}
+res.push(notarray)
+res=res.flat(Infinity)
+
+return res
+};
