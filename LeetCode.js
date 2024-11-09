e1cf3502ef80e694391272aee7cd3d0be3f0f030
ijjaasss <ijasmuthu04@gmail.com>
2024-11-09 12:47:44 +0530

    45

--- LeetCode.js
+++ LeetCode.js
@@ -2139,3 +2139,47 @@ for(let i=0;i<s.length;i++){
 }
 return res
 };
+
+
+//45
+// You are given a 0-indexed integer array nums of size 3 which can form the sides of a triangle.
+
+//     A triangle is called equilateral if it has all sides of equal length.
+//     A triangle is called isosceles if it has exactly two sides of equal length.
+//     A triangle is called scalene if all its sides are of different lengths.
+
+// Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,3,3]
+// Output: "equilateral"
+// Explanation: Since all the sides are of equal length, therefore, it will form an equilateral triangle.
+
+// Example 2:
+
+// Input: nums = [3,4,5]
+// Output: "scalene"
+
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+   var triangle =['equilateral','isosceles','scalene','none']
+if(nums[1]+nums[2]<=nums[0]||nums[0]+nums[1]<=nums[2]||nums[0]+nums[2]<=nums[1]){
+   return triangle[3]
+   
+}else if(nums[0]==nums[1]&&nums[1]==nums[2]&&nums[0]==nums[2]){
+   return triangle[0]
+   
+}else if(nums[0]==nums[1]&&nums[1]!=nums[2]||nums[0]==nums[2]&&nums[1]!=nums[2]||nums[2]==nums[1]&&nums[1]!=nums[0]){
+return triangle[1]
+
+}else{
+   return triangle[2]
+   
+}
+};
