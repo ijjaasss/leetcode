80270fad63bc20b23cc7f39c0e34664096d81fa3
ijjaasss <ijasmuthu04@gmail.com>
2024-11-09 10:50:51 +0530

    43

--- LeetCode.js
+++ LeetCode.js
@@ -2101,3 +2101,41 @@ for(let i=0;i<words.length;i++){
 }
 return result
 };
+
+
+//44
+// You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in the original string where spaces will be added. Each space should be inserted before the character at the given index.
+
+//     For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. Thus, we obtain "Enjoy Your Coffee".
+
+// Return the modified string after the spaces have been added.
+
+ 
+
+// Example 1:
+
+// Input: s = "LeetcodeHelpsMeLearn", spaces = [8,13,15]
+// Output: "Leetcode Helps Me Learn"
+// Explanation: 
+// The indices 8, 13, and 15 correspond to the underlined characters in "LeetcodeHelpsMeLearn".
+// We then place spaces before those characters.
+
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+var addSpaces = function(s, spaces) {
+   var res=''
+var l=0
+for(let i=0;i<s.length;i++){
+ if(spaces[l]==i){
+   res+=' '
+   l++
+   i--
+ } else{
+   res+=s[i]
+ } 
+}
+return res
+};
