c555c4717678d63a44008cf61a97f672b3f2da8c
ijjaasss <ijasmuthu04@gmail.com>
2024-11-15 10:19:06 +0530

    49

--- LeetCode.js
+++ LeetCode.js
@@ -2302,3 +2302,19 @@ var weaks={
 }
 return weaks[date]
 };
+
+
+
+//49
+// You are given a 0-indexed array of strings words and a character x.
+
+// Return an array of indices representing the words that contain the character x.
+
+// Note that the returned array may be in any order.
+
+ 
+
+// Example 1:
+
+// Input: words = ["leet","code"], x = "e"
+// Output: [0,1]
