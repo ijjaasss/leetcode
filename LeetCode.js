e49911e2f7b4d81519cae332187ae3765536481c
ijjaasss <ijasmuthu04@gmail.com>
2024-11-15 10:31:22 +0530

    50

--- LeetCode.js
+++ LeetCode.js
@@ -2332,3 +2332,32 @@ var findWordsContaining = function(words, x) {
    }
 return result
 };
+
+
+//50
+// You are given a 0-indexed integer array nums and a target element target.
+
+// A target index is an index i such that nums[i] == target.
+
+// Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,5,2,3], target = 2
+// Output: [1,2]
+// Explanation: After sorting, nums is [1,2,2,3,5].
+// The indices where nums[i] == 2 are 1 and 2.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function(nums, target) {
+   nums=nums.sort((a,b)=>a-b)
+var m=nums.map((val,ind)=>val==target?ind:'non')
+var result=m.filter((val)=>val!='non')
+return result
+};
