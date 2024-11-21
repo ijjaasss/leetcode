384c1ae41b5eb6122b0adcc93a17e10d167c2100
ijjaasss <ijasmuthu04@gmail.com>
2024-11-21 10:28:17 +0530

    54

--- LeetCode.js
+++ LeetCode.js
@@ -2468,3 +2468,62 @@ if(s[i].toLowerCase()!==s[i+1].toLowerCase()){
 }
 return count
 };
+
+
+
+//54
+// You are given a 0-indexed integer array nums whose length is a power of 2.
+
+// Apply the following algorithm on nums:
+
+//     Let n be the length of nums. If n == 1, end the process. Otherwise, create a new 0-indexed integer array newNums of length n / 2.
+//     For every even index i where 0 <= i < n / 2, assign the value of newNums[i] as min(nums[2 * i], nums[2 * i + 1]).
+//     For every odd index i where 0 <= i < n / 2, assign the value of newNums[i] as max(nums[2 * i], nums[2 * i + 1]).
+//     Replace the array nums with newNums.
+//     Repeat the entire process starting from step 1.
+
+// Return the last number that remains in nums after applying the algorithm.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,5,2,4,8,2,2]
+// Output: 1
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMaxGame = function(nums) {
+   if(nums.length==1){
+       return Number(nums)
+   }
+      var c=1
+  var result;
+      for(let i=0;i<nums.length/2;i+=0){
+       var temp=[]
+      for(let j=0;j<nums.length;j++){
+       if(j%2!=0){
+    if(c%2!=0){
+         temp.push(Math.min(nums[j],nums[j-1]))
+         c++
+    }else{
+        temp.push(Math.max(nums[j],nums[j-1]))
+        c++
+    }
+   }
+      }
+  
+     nums=temp 
+ 
+   if(nums.length!=0){
+       result=nums
+   }
+      
+     
+      }
+      
+    return Number(result)
+};
