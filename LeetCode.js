1f4d79bf93620910b37597bfc07c549fe4e3b6d6
ijjaasss <ijasmuthu04@gmail.com>
2024-11-25 09:50:36 +0530

    55

--- LeetCode.js
+++ LeetCode.js
@@ -2527,3 +2527,39 @@ var minMaxGame = function(nums) {
       
     return Number(result)
 };
+
+
+
+
+//55
+// Reversing an integer means to reverse all its digits.
+
+//     For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
+
+// Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
+
+ 
+
+// Example 1:
+
+// Input: num = 526
+// Output: true
+
+// Example 2:
+
+// Input: num = 1800
+// Output: false
+// Explanation: Reverse num to get 81, then reverse 81 to get 18, which does not equal num.
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isSameAfterReversals = function(num) {
+   var n=num.toString()
+var a=num.toString()
+a=a.split('').reverse().join('')
+a=parseInt(a).toString().split('').reverse().join('')
+
+return n==a
+};
