87c21ae8faa7c39e33ed247f7b5aad74c0a2918e
ijjaasss <ijasmuthu04@gmail.com>
2024-11-25 10:03:39 +0530

    56

--- LeetCode.js
+++ LeetCode.js
@@ -2563,3 +2563,38 @@ a=parseInt(a).toString().split('').reverse().join('')
 
 return n==a
 };
+
+
+//56
+// You are given an array nums consisting of positive integers.
+
+// You have to take each integer in the array, reverse its digits, and add it to the end of the array. You should apply this operation to the original integers in nums.
+
+// Return the number of distinct integers in the final array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,13,10,12,31]
+// Output: 6
+
+// Example 2:
+// Input: nums = [2,2,2]
+// Output: 1
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countDistinctIntegers = function(nums) {
+   var arr=[]
+for(let i=0;i<nums.length;i++){
+ var a=nums[i].toString().split('').reverse().join('')
+arr.push(parseInt(a))
+}
+nums.push(arr)
+nums=nums.flat(Infinity)
+var a=[...new Set(nums)]
+return a.length
+};
