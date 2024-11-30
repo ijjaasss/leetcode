8d7285d9567abc9022f7a222db1e7d3cea8b1dd7
ijjaasss <ijasmuthu04@gmail.com>
2024-11-30 12:18:37 +0530

    58

--- LeetCode.js
+++ LeetCode.js
@@ -2639,3 +2639,29 @@ return a
    
 }
 };
+
+
+//58
+// You are given a string date representing a Gregorian calendar date in the yyyy-mm-dd format.
+
+// date can be written in its binary representation obtained by converting year, month, and day to their binary representations without any leading zeroes and writing them down in year-month-day format.
+
+// Return the binary representation of date.
+
+ 
+
+// Example 1:
+
+// Input: date = "2080-02-29"
+
+// Output: "100000100000-10-11101"
+
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var convertDateToBinary = function(date) {
+   var n=date.split('-')
+var m=n.map((val)=>parseInt(val).toString(2))
+return m.join('-')
+};
