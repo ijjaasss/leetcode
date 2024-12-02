93b5706fa46b33fde37f1805393994e923d7e5e9
ijjaasss <ijasmuthu04@gmail.com>
2024-12-02 10:25:20 +0530

    61

--- LeetCode.js
+++ LeetCode.js
@@ -2730,3 +2730,31 @@ var countNegatives = function(grid) {
    a=a.filter((val)=>val<0)
    return a.length
 };
+
+
+
+//61
+// A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+// Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+   for(let i=97;i<=122;i++){
+  if(!sentence.includes(String.fromCharCode(i))){
+return false
+
+  }
+   
+}
+return true
+};
