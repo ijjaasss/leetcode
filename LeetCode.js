9630e1a0db8a75709eb1881725a79bce405b5a9f
ijjaasss <ijasmuthu04@gmail.com>
2024-12-06 10:53:07 +0530

    62

--- LeetCode.js
+++ LeetCode.js
@@ -2758,3 +2758,41 @@ return false
 }
 return true
 };
+
+
+//62
+// You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+// The ith item is said to match the rule if one of the following is true:
+
+//     ruleKey == "type" and ruleValue == typei.
+//     ruleKey == "color" and ruleValue == colori.
+//     ruleKey == "name" and ruleValue == namei.
+
+// Return the number of items that match the given rule.
+
+ 
+
+// Example 1:
+
+// Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+// Output: 1
+
+
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+   var a=["type", "color", "name"]
+a=a.indexOf(ruleKey)
+var result=[]
+for(let i=0;i<items.length;i++){
+   if(items[i][a]==ruleValue){
+result.push(items[i])
+   }
+}
+return result.length
+};
