e21e83b5765bc304c9b9de715620f00b0b233103
ijjaasss <ijasmuthu04@gmail.com>
2024-12-06 11:06:25 +0530

    63

--- LeetCode.js
+++ LeetCode.js
@@ -2796,3 +2796,44 @@ result.push(items[i])
 }
 return result.length
 };
+
+
+//63
+// A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
+
+//     s[i] == 'I' if perm[i] < perm[i + 1], and
+//     s[i] == 'D' if perm[i] > perm[i + 1].
+
+// Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.
+
+ 
+
+// Example 1:
+
+// Input: s = "IDID"
+// Output: [0,4,1,3,2]
+
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var diStringMatch = function(s) {
+   var m=0
+var d=s.length
+var arr=[]
+for(let i=0;i<s.length;i++){
+   if(s[i]=='I'){
+       arr.push(m++)
+   }else{
+       arr.push(d--)
+   }
+}
+
+for(let i=0;i<=s.length;i++){
+  if(!arr.includes(i)){
+arr.push(i)
+  }
+}
+return arr
+
+};
