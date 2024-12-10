4b4a3dc5164bdaeab84c4be5f126e2bbf400cc29
ijjaasss <ijasmuthu04@gmail.com>
2024-12-11 00:14:41 +0530

    69

--- LeetCode.js
+++ LeetCode.js
@@ -3012,3 +3012,30 @@ var smallerNumbersThanCurrent = function(nums) {
  }
  return arr
  };
+
+
+ //69
+//  A peak element is an element that is strictly greater than its neighbors.
+
+// Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+// You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+// You must write an algorithm that runs in O(log n) time.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+   var a=Math.max(...nums)
+   return nums.indexOf(a)
+};
+
