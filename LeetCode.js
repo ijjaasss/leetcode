506f22f0818fc7f7780342459639e1f6b282cb08
ijjaasss <ijasmuthu04@gmail.com>
2024-12-12 10:28:20 +0530

    71

--- LeetCode.js
+++ LeetCode.js
@@ -3067,4 +3067,59 @@ var removeTrailingZeros = function(num) {
 a=BigInt(a)
 a=a.toString().split('').reverse().join('')
 return a
+
+};
+
+//71
+// You are given a string s consisting of lowercase English letters, and an integer k. Your task is to convert the string into an integer by a special process, and then transform it by summing its digits repeatedly k times. More specifically, perform the following steps:
+
+//     Convert s into an integer by replacing each letter with its position in the alphabet (i.e. replace 'a' with 1, 'b' with 2, ..., 'z' with 26).
+//     Transform the integer by replacing it with the sum of its digits.
+//     Repeat the transform operation (step 2) k times in total.
+
+// For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+
+//     Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+//     Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+//     Transform #2: 17 ➝ 1 + 7 ➝ 8
+
+// Return the resulting integer after performing the operations described above.
+
+ 
+
+// Example 1:
+
+// Input: s = "iiii", k = 1
+
+// Output: 36
+
+// Explanation:
+
+// The operations are as follows:
+// - Convert: "iiii" ➝ "(9)(9)(9)(9)" ➝ "9999" ➝ 9999
+// - Transform #1: 9999 ➝ 9 + 9 + 9 + 9 ➝ 36
+// Thus the resulting integer is 36.
+
+// Example 2:
+
+// Input: s = "leetcode", k = 2
+
+// Output: 6
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var getLucky = function(s, k) {
+   var arr=s.split('').map((val)=>val.charCodeAt()-96)
+   arr=arr.join('').split('').map((val)=>Number(val))
+var sum=0
+for(let i=0;i<k;i++){
+  sum=arr.reduce((acc,val)=>acc+val,0)
+  arr=sum.toString().split('').map((val)=>Number(val))
+ 
+}
+return sum
+console.log(sum);
 };
