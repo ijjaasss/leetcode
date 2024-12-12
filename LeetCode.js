d26dc93ba960220cada5b069d8cd31cdb8afecd4
ijjaasss <ijasmuthu04@gmail.com>
2024-12-12 09:56:10 +0530

    70

--- LeetCode.js
+++ LeetCode.js
@@ -3039,3 +3039,32 @@ var findPeakElement = function(nums) {
    return nums.indexOf(a)
 };
 
+
+
+
+//70
+// Given a positive integer num represented as a string, return the integer num without trailing zeros as a string.
+
+ 
+
+// Example 1:
+
+// Input: num = "51230100"
+// Output: "512301"
+// Explanation: Integer "51230100" has 2 trailing zeros, we remove them and return integer "512301".
+
+// Example 2:
+
+// Input: num = "123"
+// Output: "123"
+
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var removeTrailingZeros = function(num) {
+   var a=num.split('').reverse().join('')
+a=BigInt(a)
+a=a.toString().split('').reverse().join('')
+return a
+};
