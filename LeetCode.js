4f29626569f866554ef4e57043c3f1e80b90f961
ijjaasss <ijasmuthu04@gmail.com>
2024-12-16 23:44:33 +0530

    77

--- LeetCode.js
+++ LeetCode.js
@@ -3391,3 +3391,42 @@ for(let i=0;i<nums.length;i++){
 }
 return res
 };
+
+//77
+// You are given an integer array nums containing positive integers. We define a function encrypt such that encrypt(x) replaces every digit in x with the largest digit in x. For example, encrypt(523) = 555 and encrypt(213) = 333.
+
+// Return the sum of encrypted elements.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+
+// Output: 6
+
+// Explanation: The encrypted elements are [1,2,3]. The sum of encrypted elements is 1 + 2 + 3 == 6.
+
+// Example 2:
+
+// Input: nums = [10,21,31]
+
+// Output: 66
+
+// Explanation: The encrypted elements are [11,22,33]. The sum of encrypted elements is 11 + 22 + 33 == 66.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfEncryptedInt = function(nums) {
+   var sum=0
+for(let i=0;i<nums.length;i++){
+   var temp=nums[i].toString().split('').map(Number)
+ var max=Math.max(...temp).toString()
+ var num=max.repeat(temp.length)
+ sum+=parseInt(num)
+ 
+   
+}
+return sum
+};
