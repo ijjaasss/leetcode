9d5e3c4ba92256f0881cd3a7edee5596383f2711
ijjaasss <ijasmuthu04@gmail.com>
2024-12-16 14:23:31 +0530

    72

--- LeetCode.js
+++ LeetCode.js
@@ -3123,3 +3123,47 @@ for(let i=0;i<k;i++){
 return sum
 console.log(sum);
 };
+
+
+//72
+// You are given an integer array nums, an integer k, and an integer multiplier.
+
+// You need to perform k operations on nums. In each operation:
+
+//     Find the minimum value x in nums. If there are multiple occurrences of the minimum value, select the one that appears first.
+//     Replace the selected minimum value x with x * multiplier.
+
+// Return an integer array denoting the final state of nums after performing all k operations.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,1,3,5,6], k = 5, multiplier = 2
+
+// Output: [8,4,6,5,6]
+
+// Explanation:
+// Operation	Result
+// After operation 1	[2, 2, 3, 5, 6]
+// After operation 2	[4, 2, 3, 5, 6]
+// After operation 3	[4, 4, 3, 5, 6]
+// After operation 4	[4, 4, 6, 5, 6]
+// After operation 5	[8, 4, 6, 5, 6]
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+var getFinalState = function(nums, k, multiplier) {
+   for(let  i=1;i<=k;i++){
+   var small=Math.min(...nums)
+   var ind=nums.indexOf(small)
+   nums[ind]=nums[ind]*multiplier
+ 
+}
+return nums
+};
