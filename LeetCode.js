698d92ca909fb0a0b689d8d628e4a81e0dcb8ebd
ijjaasss <ijasmuthu04@gmail.com>
2024-12-16 15:12:02 +0530

    74

--- LeetCode.js
+++ LeetCode.js
@@ -3214,3 +3214,47 @@ var getSneakyNumbers = function(nums) {
    var n=nums.filter((val,ind)=>nums.indexOf(val)!=ind)
    return n
 };
+
+
+//74
+// You are given an integer array nums.
+
+// You replace each element in nums with the sum of its digits.
+
+// Return the minimum element in nums after all replacements.
+
+ 
+
+// Example 1:
+
+// Input: nums = [10,12,13,14]
+
+// Output: 1
+
+// Explanation:
+
+// nums becomes [1, 3, 4, 5] after all replacements, with minimum element 1.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: 1
+
+// Explanation:
+
+// nums becomes [1, 2, 3, 4] after all replacements, with minimum element 1.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minElement = function(nums) {
+   var arr=[]
+for(let i=0;i<nums.length;i++){
+   var a=nums[i].toString()
+   var sum=a.split('').reduce((acc,val)=>acc+Number(val),0)
+  arr.push(sum)
+   
+}
+return Math.min(...arr)
+};
