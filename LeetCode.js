9fc01fce4e09f0ccb7d09ed5816799e0063c983c
ijjaasss <ijasmuthu04@gmail.com>
2024-12-16 22:56:17 +0530

    76

--- LeetCode.js
+++ LeetCode.js
@@ -3297,3 +3297,41 @@ if(i%3==0||i%5==0||i%7==0){
    }
    return sum
 };
+
+//76
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+ /**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+   var sum=0
+var arr=[]
+for(let i=0;i<nums.length;i++){
+  sum+=nums[i]
+  arr.push(sum)
+}
+return arr
+};
